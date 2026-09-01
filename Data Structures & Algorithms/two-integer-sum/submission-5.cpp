fa3adacfa15a52e0709d26c0dbc5b469ebd756dc
Gaurav Kanwat <56406342+GauravKanwat@users.noreply.github.com>
2026-09-01 21:44:27 +0530

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> mpp;
+        for(int i=0; i<nums.size(); i++) {
+            int rem = target - nums[i];
+            auto it = mpp.find(rem);
+            if(it != mpp.end()) return {it->second, i};
+            mpp[nums[i]] = i;
+        }
+        return {};
+    }
+};
